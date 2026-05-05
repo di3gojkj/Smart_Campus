1fbcab2025be0e6f7e840fe245b54c8435363e7b
di3gojkj <diego.rivas.varas@gmail.com>
2026-05-05 14:31:13 -0400

    Microservicio Gestion de Estado
    se crea el primer microservicio llamada Gestion de estado en springboot con sus respectivas librerias.

---
+++ Ms_Gestion_Estado/Ms_Gestion_Estado/src/main/java/com/diego/Ms_Gestion_Estado/MsGestionEstadoApplication.java
@@ -0,0 +1,13 @@
+package com.diego.Ms_Gestion_Estado;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsGestionEstadoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsGestionEstadoApplication.class, args);
+	}
+
+}
---
+++ Ms_Gestion_Estado/Ms_Gestion_Estado/src/test/java/com/diego/Ms_Gestion_Estado/MsGestionEstadoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.diego.Ms_Gestion_Estado;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsGestionEstadoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
