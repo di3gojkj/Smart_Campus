8335cd3d404648d2a7f266b113b874f660023475
Elkriu <Cri.bustamanteb@duocuc.cl>
2026-05-05 22:53:04 -0400

    Ms_Asignatura
    Empezando codigo del micro servicio Asignatura.

---
+++ Ms_Asignatura/msAsignatura/src/main/java/com/smartcampus/msAsignatura/MsAsignaturaApplication.java
@@ -0,0 +1,13 @@
+package com.smartcampus.msAsignatura;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsAsignaturaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsAsignaturaApplication.class, args);
+	}
+
+}
---
+++ Ms_Asignatura/msAsignatura/src/test/java/com/smartcampus/msAsignatura/MsAsignaturaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.smartcampus.msAsignatura;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsAsignaturaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
