d9a5059c52931fda64efca12d06694841cdb5814
Elkriu <Cri.bustamanteb@duocuc.cl>
2026-05-06 21:09:21 -0400

    Creacion micro servicio Ms_Evaluacion
    Se integro el ms, carpetas necesarias y application propeties

---
+++ Ms_Evaluacion/Ms_Evaluacion/src/test/java/com/SmartCampus/Ms_Evaluacion/MsEvaluacionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.SmartCampus.Ms_Evaluacion;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsEvaluacionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
