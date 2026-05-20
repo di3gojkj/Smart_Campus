ca1878ba489762bc2fb5ec2f36284c68371229ef
di3gojkj <diego.rivas.varas@gmail.com>
2026-05-19 22:52:50 -0400

    Repository
    se agrega una consukta jpql de ejemplo para la defensas academicas con su respectivo query

--- Ms_Gestion_Estado/Ms_Gestion_Estado/src/main/java/com/diego/Ms_Gestion_Estado/repository/EstadoRepository.java
+++ Ms_Gestion_Estado/Ms_Gestion_Estado/src/main/java/com/diego/Ms_Gestion_Estado/repository/EstadoRepository.java
@@ -3,6 +3,8 @@ package com.diego.Ms_Gestion_Estado.repository;
 import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import com.diego.Ms_Gestion_Estado.model.Estado;
@@ -10,6 +12,10 @@ import com.diego.Ms_Gestion_Estado.model.Estado;
 @Repository
 public interface EstadoRepository extends JpaRepository<Estado, Long> {//El jpaRepository de spring jpa regala metodos CRUD: save, findAll, findById, Delete.
     Optional<Estado> findByNombreIgnoreCase(String nombre);//Metodo que Busca estados por su nombre Ignorando las mayusculas.
-    //Optional se usa como buena practica para que no de un error en caso de que el nombre de estado no exista. 
+    //Optional se usa como buena practica para que no de un error en caso de que el nombre de estado no exista.
+    
+    // Consulta JPQL de ejemplo para defensas académicas (Mapea a la Entidad Estado, no a la tabla)
+    @Query("SELECT e FROM Estado e WHERE LOWER(e.nombre) = LOWER(:nombre)")
+    Optional<Estado> buscarPorNombreExacto(@Param("nombre") String nombre);
 
 }
