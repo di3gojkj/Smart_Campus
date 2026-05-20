bc439cbb913822491e2065de20bda81cb4a28774
di3gojkj <diego.rivas.varas@gmail.com>
2026-05-19 22:55:31 -0400

    Update EstadoRequestDTO
    se agrega el size para indicar que el nombre no puede superar los 50 caracteres.

--- Ms_Gestion_Estado/Ms_Gestion_Estado/src/main/java/com/diego/Ms_Gestion_Estado/dto/EstadoRequestDTO.java
+++ Ms_Gestion_Estado/Ms_Gestion_Estado/src/main/java/com/diego/Ms_Gestion_Estado/dto/EstadoRequestDTO.java
@@ -1,10 +1,16 @@
 package com.diego.Ms_Gestion_Estado.dto;
 
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class EstadoRequestDTO {
     @NotBlank(message = "El nombre del estado es obligatorio")
+    @Size(max = 50, message = "El nombre del estado no puede superar los 50 caracteres")
     private String nombre;
 }
