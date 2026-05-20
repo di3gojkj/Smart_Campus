8b9e642305bac1dd24cada0d73158cfd1753158a
di3gojkj <diego.rivas.varas@gmail.com>
2026-05-20 00:24:19 -0400

    Microservicio Gestion de usuario
    Se crea el microservicio gestion de usuarios con todas sus librerias desde springboot incluyendo open feign

---
+++ MS_Gestión_Usuario/MS_Gestión_Usuario/src/main/java/com/diego/MS_Gestion_Usuario/MsGestionUsuarioApplication.java
@@ -0,0 +1,13 @@
+package com.diego.MS_Gestion_Usuario;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsGestionUsuarioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsGestionUsuarioApplication.class, args);
+	}
+
+}
---
+++ MS_Gestión_Usuario/MS_Gestión_Usuario/src/test/java/com/diego/MS_Gestion_Usuario/MsGestionUsuarioApplicationTests.java
@@ -0,0 +1,13 @@
+package com.diego.MS_Gestion_Usuario;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsGestionUsuarioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
