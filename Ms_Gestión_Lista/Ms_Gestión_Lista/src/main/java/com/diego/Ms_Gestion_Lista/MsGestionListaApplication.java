0f3af0906e5874878892a140af4e8b7fe3469026
di3gojkj <diego.rivas.varas@gmail.com>
2026-05-20 01:37:46 -0400

    Microservicio Gestion Lista
    Se crea el micro servicio en springboot integrando el feignclient

---
+++ Ms_Gestión_Lista/Ms_Gestión_Lista/src/main/java/com/diego/Ms_Gestion_Lista/MsGestionListaApplication.java
@@ -0,0 +1,13 @@
+package com.diego.Ms_Gestion_Lista;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsGestionListaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsGestionListaApplication.class, args);
+	}
+
+}
---
+++ Ms_Gestión_Lista/Ms_Gestión_Lista/src/test/java/com/diego/Ms_Gestion_Lista/MsGestionListaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.diego.Ms_Gestion_Lista;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsGestionListaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
