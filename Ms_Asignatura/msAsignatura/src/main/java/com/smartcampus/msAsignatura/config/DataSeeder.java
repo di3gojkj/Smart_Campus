d81a7927c766c733b6d55c058dafac13d3f3ccf1
Elkriu <Cri.bustamanteb@duocuc.cl>
2026-06-13 19:51:35 -0400

    Se terminaron los cambios a main de Ms_Asignatura..

---
+++ Ms_Asignatura/msAsignatura/src/main/java/com/smartcampus/msAsignatura/config/DataSeeder.java
@@ -0,0 +1,87 @@
+package com.smartcampus.msAsignatura.config;
+
+import java.util.List;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Profile;
+import org.springframework.stereotype.Component;
+
+import com.smartcampus.msAsignatura.model.Asignatura;
+import com.smartcampus.msAsignatura.model.Semestre;
+import com.smartcampus.msAsignatura.repository.AsignaturaRepository;
+import com.smartcampus.msAsignatura.repository.SemestreRepository;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+@Component
+@Profile("!test")
+@RequiredArgsConstructor
+public class DataSeeder implements CommandLineRunner {
+
+    private final AsignaturaRepository asignaturaRepository;
+    private final SemestreRepository semestreRepository;
+
+    @Override
+    public void run(String... args) throws Exception {
+        log.info("[DataSeeder]: Insertando Semestres...");
+
+        
+        if (semestreRepository.count() == 0) {
+            
+            Semestre s1 = new Semestre();
+            s1.setNombre("2026-1");
+            s1.setIdEstado(1L); 
+
+            Semestre s2 = new Semestre();
+            s2.setNombre("2026-2");
+            s2.setIdEstado(1L);
+
+            semestreRepository.saveAll(List.of(s1, s2));
+            log.info("[DataSeeder]: Datos iniciales de semestre cargados con exito");
+        } else {
+            log.info("[DataSeeder]: La tabla ya contiende datos, omitiendo carga");
+        }
+
+        
+        if (asignaturaRepository.count() == 0) {
+            log.info("[DataSeeder]: Insertando Asignaturas...");
+
+            Asignatura a1 = new Asignatura();
+            a1.setNombre("Base de Datos I");
+            a1.setSigla("INF-230");
+            a1.setIdEstado(1L); 
+
+            Asignatura a2 = new Asignatura();
+            a2.setNombre("Desarrollo En Fullstack");
+            a2.setSigla("INF-420");
+            a2.setIdEstado(1L);
+
+            Asignatura a3 = new Asignatura();
+            a3.setNombre("Autotronica");
+            a3.setSigla("MCA-510");
+            a3.setIdEstado(1L);
+
+            Asignatura a4 = new Asignatura();
+            a4.setNombre("Seguridad ocupacional");
+            a4.setSigla("MCA-310");
+            a4.setIdEstado(1L);
+
+            asignaturaRepository.saveAll(List.of(a1, a2, a3, a4));
+            log.info("[DataSeeder]: Datos iniciales de asignatura cargados con exito");
+        } else {
+            log.info("[DataSeeder]: La tabla ya contiende datos, omitiendo carga");
+        }
+
+        log.info("[DataSeeder]: Proceso de inicializacion de datos finalizado correctamente.");
+    }
+} 
+
+
+
+
+
+    
+
+
