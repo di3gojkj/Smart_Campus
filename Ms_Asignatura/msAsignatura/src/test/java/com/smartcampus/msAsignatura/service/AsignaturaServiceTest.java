6ce3d080a5c41930231aa166127a9955b2517903
Elkriu <Cri.bustamanteb@duocuc.cl>
2026-06-15 20:46:41 -0400

    cambios menores

--- Ms_Asignatura/msAsignatura/src/test/java/com/smartcampus/msAsignatura/service/AsignaturaServiceTest.java
+++ Ms_Asignatura/msAsignatura/src/test/java/com/smartcampus/msAsignatura/service/AsignaturaServiceTest.java
@@ -3,7 +3,6 @@ package com.smartcampus.msAsignatura.service;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertThrows;
-import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.never;
 import static org.mockito.Mockito.times;
