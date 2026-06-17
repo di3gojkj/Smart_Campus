577d30ab8868fe3343b2a3ca0535cd22f6f0025a
di3gojkj <diego.rivas.varas@gmail.com>
2026-06-17 04:01:15 -0400

    DTO Response
    se aplica la documentacion olvidada en los dtos response. Asi la logica solicitada por el docente cubre el 80% del proyecto.

--- Ms_Gestión_Lista/Ms_Gestión_Lista/src/main/java/com/diego/Ms_Gestion_Lista/dto/CalificacionResponseDTO.java
+++ Ms_Gestión_Lista/Ms_Gestión_Lista/src/main/java/com/diego/Ms_Gestion_Lista/dto/CalificacionResponseDTO.java
@@ -1,5 +1,6 @@
 package com.diego.Ms_Gestion_Lista.dto;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -8,9 +9,18 @@ import java.math.BigDecimal;
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
+@Schema(description = "Objeto de respuesta que devuelve el detalle de una nota ingresada")
 public class CalificacionResponseDTO {
+    
+    @Schema(description = "ID autogenerado de la calificación", example = "1")
     private Long idCalificacion;
+    
+    @Schema(description = "Nota obtenida por el alumno", example = "6.5")
     private BigDecimal nota;
+    
+    @Schema(description = "ID de la inscripción a la que pertenece la nota", example = "1")
     private Long idLista;
+    
+    @Schema(description = "ID de la evaluación original del curso", example = "2")
     private Long idCurEva;
 }
--- Ms_Gestión_Lista/Ms_Gestión_Lista/src/main/java/com/diego/Ms_Gestion_Lista/dto/ListaResponseDTO.java
+++ Ms_Gestión_Lista/Ms_Gestión_Lista/src/main/java/com/diego/Ms_Gestion_Lista/dto/ListaResponseDTO.java
@@ -1,5 +1,6 @@
 package com.diego.Ms_Gestion_Lista.dto;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -8,9 +9,18 @@ import java.time.LocalDateTime;
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
+@Schema(description = "Objeto de respuesta que devuelve los datos de la inscripción")
 public class ListaResponseDTO {
+    
+    @Schema(description = "ID autogenerado de la lista", example = "1")
     private Long idLista;
+    
+    @Schema(description = "ID del usuario (Estudiante)", example = "10")
     private Long idUser;
+    
+    @Schema(description = "ID del curso inscrito", example = "5")
     private Long idCurso;
+    
+    @Schema(description = "Fecha exacta en la que se generó la inscripción", example = "2026-06-17T10:30:00")
     private LocalDateTime fechaCreacion;
 }
