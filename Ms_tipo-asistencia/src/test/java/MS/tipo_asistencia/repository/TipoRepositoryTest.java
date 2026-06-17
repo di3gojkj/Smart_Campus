761fc3a7fab5feb865f561042629a0411b2cf2e5
MarioMG27 <mar.mardonesg@duocuc.cl>
2026-06-17 18:14:31 -0400

    Ms_tipo-asistencia corregido
    se agregan propiedades a dataJpaTest del test repositorio y se actualiza el ap-test.properties

--- Ms_tipo-asistencia/src/test/java/MS/tipo_asistencia/repository/TipoRepositoryTest.java
+++ Ms_tipo-asistencia/src/test/java/MS/tipo_asistencia/repository/TipoRepositoryTest.java
@@ -18,7 +18,11 @@ import org.springframework.test.context.ActiveProfiles;
 
 import MS.tipo_asistencia.model.Tipo; // CORREGIDO: Importa 'Tipo'
 
-@DataJpaTest
+@DataJpaTest(properties = {
+    "spring.jpa.database-platform=org.hibernate.dialect.H2Dialect",
+    "spring.jpa.properties.hibernate.dialect=org.hibernate.dialect.H2Dialect",
+    "spring.datasource.url=jdbc:h2:mem:testdb;MODE=MySQL;IGNORECASE=TRUE"
+})
 @ActiveProfiles("test")
 @DisplayName("Test del repositorio de tipos de asistencia en memoria H2")
 public class TipoRepositoryTest {
