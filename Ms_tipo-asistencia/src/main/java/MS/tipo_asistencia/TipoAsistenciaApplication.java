f42fb2ebd4c20462f396b03ec851ac4825c8a787
Elkriu <Cri.bustamanteb@duocuc.cl>
2026-06-21 19:35:15 -0400

    Cambios menores

--- Ms_tipo-asistencia/src/main/java/MS/tipo_asistencia/TipoAsistenciaApplication.java
+++ Ms_tipo-asistencia/src/main/java/MS/tipo_asistencia/TipoAsistenciaApplication.java
@@ -2,8 +2,10 @@ package MS.tipo_asistencia;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
+@EnableFeignClients
 public class TipoAsistenciaApplication {
 
 	public static void main(String[] args) {
