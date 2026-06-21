3e1efd9ded9a77c02f7c7bfb218c571eb22aab2f
Elkriu <Cri.bustamanteb@duocuc.cl>
2026-06-21 12:49:38 -0400

    Cambios menores

--- MS_Gestión_Usuario/MS_Gestión_Usuario/src/main/java/com/diego/MS_Gestion_Usuario/config/DataInitializer.java
+++ MS_Gestión_Usuario/MS_Gestión_Usuario/src/main/java/com/diego/MS_Gestion_Usuario/config/DataInitializer.java
@@ -1,7 +1,6 @@
 package com.diego.MS_Gestion_Usuario.config;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Component;
@@ -13,14 +12,17 @@ import com.diego.MS_Gestion_Usuario.repository.RolRepository;
 import com.diego.MS_Gestion_Usuario.repository.UsuarioRepository;
 
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+
 import java.util.HashSet;
 import java.util.List;
 
+@Slf4j
 @Component
 @RequiredArgsConstructor
 public class DataInitializer implements CommandLineRunner {
 
-    private static final Logger logger = LoggerFactory.getLogger(DataInitializer.class);
+    
     
     private final RolRepository rolRepository;
     private final UsuarioRepository usuarioRepository;
@@ -31,24 +33,24 @@ public class DataInitializer implements CommandLineRunner {
     public void run(String... args) {
         
         if (rolRepository.count() > 0) {
-            logger.info("DataInitializer: Estructura de roles ya detectada en la BD. Omitiendo populado.");
+            log.info("DataInitializer: Estructura de roles ya detectada en la BD. Omitiendo populado.");
         } else {
-            logger.info("DataInitializer: Base de datos de roles vacía. Iniciando inserción de roles universitarios...");
+            log.info("DataInitializer: Base de datos de roles vacía. Iniciando inserción de roles universitarios...");
             try {
                 rolRepository.save(new Rol(null, "ADMINISTRADOR", 1L));
                 rolRepository.save(new Rol(null, "DOCENTE", 1L));
                 rolRepository.save(new Rol(null, "ESTUDIANTE", 1L));
-                logger.info("Roles cargados exitosamente. Total registros: {}", rolRepository.count());
+                log.info("Roles cargados exitosamente. Total registros: {}", rolRepository.count());
             } catch (Exception e) {
-                logger.error("No se pudieron inicializar los roles: {}", e.getMessage());
+                log.error("No se pudieron inicializar los roles: {}", e.getMessage());
             }
         }
 
     
         if (usuarioRepository.count() > 0) {
-            logger.info("Estructura de usuarios ya detectada en la BD. Omitiendo populado.");
+            log.info("Estructura de usuarios ya detectada en la BD. Omitiendo populado.");
         } else {
-            logger.info("Base de datos de usuarios vacía. Iniciando inserción de cuentas maestras...");
+            log.info("Base de datos de usuarios vacía. Iniciando inserción de cuentas maestras...");
             try {
                 Usuario admin = new Usuario(
                         null, 
@@ -74,9 +76,9 @@ public class DataInitializer implements CommandLineRunner {
 
                 usuarioRepository.save(admin);
                 usuarioRepository.save(estudiante);
-                logger.info("Usuarios cargados exitosamente. Total registros: {}", usuarioRepository.count());
+                log.info("Usuarios cargados exitosamente. Total registros: {}", usuarioRepository.count());
             } catch (Exception e) {
-                logger.error("No se pudieron inicializar los usuarios: {}", e.getMessage());
+                log.error("No se pudieron inicializar los usuarios: {}", e.getMessage());
             }
         }
     }
